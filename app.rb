361a0ea17d129941ea8f75bfb5a05257c7f3ec18
Connie Hyman <constance.hyman@mchsi.com>
2018-12-16 09:12:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
   end
 
   post "/puppy" do
-    np = Puppy.new(params['name'], params['breed'] params['age'])
+    np = Puppy.new(params['name'], params['breed'], params['age'])
     erb :display_puppy
   end
 end
